2a611c7020ebf53a0470fa8bf40d5a0fe08e14f6
丁敬 <dingjing@live.cn>
2018-11-27 14:48:58 +0800

    主页布局

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,120 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- 顶部导航栏 -->
+    <div class="i-main">
+      <!-- 图标 -->
+      <div class="i-main-logo">
+        <img alt="logo" src=""/>
+      </div>
+      <!-- 标题 -->
+      <div class="i-main-title">
+        <!-- -->
+        <h2>宜搜信息查询</h2>
+      </div>
+      <!-- 搜索框 -->
+      <div class="i-main-search">
+        <div style="margin-top:19px;">
+          <el-input placeholder="物品信息查找 ..." v-model="itemSearch" class="input-with-select">
+            <el-select v-model="select" slot="prepend" placeholder="维度" value="">
+              <el-option label="书籍ID" value="1"></el-option>
+              <el-option label="书籍名" value="2"></el-option>
+              <el-option label="作者名" value="3"></el-option>
+            </el-select>
+            <el-button slot="append" icon="el-icon-search"></el-button>
+          </el-input>
+        </div>
+      </div>
+      <div class="clear"></div>
+      <hr style="width:100%;height:1px;margin-top:16px;border:none;background-color:#E0E0E0;">
+    </div>
+    <!-- 左侧导航栏 -->
+    <MainMenu></MainMenu>
   </div>
 </template>
 
 <script>
+import MainMenu from '@/components/MainMenu'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      itemSearch: '',
+      select: '1'
+    }
+  },
+  methods: {
+
+  },
+  components: {
+    MainMenu
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  *,div,img,html,head,body,header,ul,li,a,hr,el-option,el-input,h1,h2,h3,h4,h5,h6,el-menu,el-menu-item{
+    padding: 0;
+    margin: 0;
+  }
+  body{
+    width: 100%;
+    height: 100%;
+    min-width: 1200px;
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  #app {
+    width: 100%;
+    height: 100%;
+    min-width: 1200px;
+    min-height: 800px;
+    font-family: Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0;
+  }
+  .i-main {
+    width: 100%;
+    height: 78px;
+    min-width: 1200px;
+    position: fixed;
+  }
+  .i-main-title {
+    width: 160px;
+    margin-top: 18px;
+    margin-left: 32%;
+    float: left;
+    font-family: 楷体, sans-serif;
+  }
+  .i-main-search{
+    width: 360px;
+    margin-right: 32px;
+    float: right;
+  }
+  .i-main-logo {
+    float: left;
+  }
+  .i-main-logo>img {
+    width: 180px;
+    height: 56px;
+    margin: 3px 28px;
+  }
+  .el-select .el-input {
+    width: 90px;
+  }
+  .input-with-select .el-input-group__prepend {
+    background-color: #fff;
+  }
+  .clear{
+    clear:both;
+    height: 0;
+    line-height: 0;
+    font-size: 0
+  }
 </style>
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,90 @@
+<template>
+  <el-menu class="i-main-menu" v-model="isCollapse" :collapse="isCollapse">
+    <!-- 导航1 -- 开始 -->
+    <el-menu-item index="1" v-on:click="isCollapse = !isCollapse">
+        <i class="el-icon-menu"></i>
+        <span slot="title">菜单</span>
+    </el-menu-item>
+    <!-- 导航1 -- 结束 -->
+    <!-- -->
+    <!-- 导航2 -- 开始 -->
+    <el-submenu index="2">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>订展比统计</span>
+      </template>
+      <el-menu-item index="2-1">比例查询</el-menu-item>
+      <el-menu-item index="2-2">数量查询</el-menu-item>
+    </el-submenu>
+    <!-- 导航2 -- 结束 -->
+    <!-- 导航3 -- 开始 -->
+    <el-submenu index="3">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>阅读相关统计</span>
+      </template>
+      <el-menu-item-group index="3-1">
+        <template slot="title">留存统计</template>
+        <el-menu-item index="3-1-1">留存量</el-menu-item>
+        <el-menu-item index="3-1-2">留存率</el-menu-item>
+      </el-menu-item-group>
+      <!-- -->
+      <el-menu-item-group title="3-2">
+        <template slot="title">天阅读统计</template>
+        <el-menu-item index="3-2-2">天阅读量</el-menu-item>
+        <el-menu-item index="3-2-1">天付费阅读量</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <!-- 导航2 -- 结束 -->
+    <!-- -->
+    <!-- 导航3 -- 开始 -->
+    <el-submenu index="4">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span>用户类型统计</span>
+      </template>
+      <el-menu-item index="4-1">用户类型数量</el-menu-item>
+      <el-menu-item index="4-2">用户类型占比</el-menu-item>
+    </el-submenu>
+    <!-- 导航3 -- 结束 -->
+    <!-- -->
+  </el-menu>
+</template>
+
+<style>
+  .i-main-menu {
+    top: 84px;
+    position: fixed;
+  }
+  .i-main-menu>el-menu-item{
+    height: 100%;
+    background-color: #8c0776;
+  }
+  .i-main-menu:not(.el-menu--collapse) {
+    width: 200px;
+    height: 100%;
+    min-height: 400px;
+  }
+</style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      isCollapse: true
+    }
+  },
+  methods: {
+    handleOpen (key, keyPath) {
+      alert(key)
+      if (key === 1) {
+        alert('ddd')
+      }
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
