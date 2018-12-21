41c30cdd6bde1262d390f8d0ac12b164a1ee8a0f
丁敬 <dingjing@live.cn>
2018-12-21 13:34:23 +0800

    修改ip

--- src/pages/MainExhibit.vue
+++ src/pages/MainExhibit.vue
@@ -372,7 +372,7 @@ export default {
         }
         request['timeRange'] = this.form.timeRange
         axios({
-          url: 'http://10.100.2.118:32000/exhibit',
+          url: 'http://10.26.26.161:32000/exhibit',
           method: 'post',
           data: request,
           headers: {
